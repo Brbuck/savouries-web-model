25b367d53e58c71285012b431a4ece5ab131ca6a
Bruno <bruno.buck1@hotmail.com>
2022-06-28 16:44:34 -0300

    Update Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-// import { Container } from './styles';
+import './styles.scss';
 
 import Swiper from "../../components/Swiper";
 import ForYou from "../../components/Menu";
 import CenterBranded from "../../Components/CenterBranded";
 
-
 import { Data } from "./DataServices";
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Swiper />
       <ForYou
         data={Data}
@@ -24,6 +23,16 @@ function Home() {
         }
         link={"Converse com a gente"}
       />
+      <section className='widget'>
+        <h1>Comemorar ficou mais importante do que nunca!</h1>
+        <p>
+          O mundo aprendeu a valorizar a vida, as pessoas e a importância de
+          cada segundo. Nossa missão ficou mais séria ainda, ao ajudar você a
+          celebrar a vida e desfrutar do que mais importa: o amor uns pelos
+          outros. Não deixe passar em branco, comemore!
+        </p>
+        <p>Salgados Maravilha</p>
+      </section>
     </div>
   );
 }
